07c922959a699ebd315689b624a02916c7a99b3c
Eric Hodges <ehodges2004@gmail.com>
2020-07-02 14:52:39 -0700

    Content: Added more yoga sutra pages

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -63,7 +63,7 @@ export default {
     },
     display: {
       variant: 'text.heading',
-      fontSize: [5, 6],
+      fontSize: [4, 5],
       fontWeight: 'display',
       letterSpacing: '-0.03em',
       mt: 3,
