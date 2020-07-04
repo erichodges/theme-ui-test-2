db464e2ad8a9a2fe085132a9572ad994a51fe5a0
Eric Hodges <ehodges2004@gmail.com>
2020-07-04 14:12:40 -0700

    Layout: Updated sidebar and added content

--- src/components/edit-link.js
+++ src/components/edit-link.js
@@ -10,9 +10,9 @@ const getHREF = (base, location) => {
 
   if (location.pathname === '/recipes') return `${basePagesHref}/index.js`
 
-  if (location.pathname === '/Chapter-1') return `${basePagesHref}/index.mdx`
+  if (location.pathname === '/guides') return `${basePagesHref}/index.mdx`
 
-  if (location.pathname.startsWith('/Chapter-1/') === false)
+  if (location.pathname.startsWith('/recipes/') === false)
     return `${basePagesHref}.mdx`
 
   return `${base}${sanitizedLocationPathname}.mdx`
