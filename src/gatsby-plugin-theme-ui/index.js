5ae502d7b76f1afa328f134bba53b424da8fcd3b
Eric Hodges <ehodges2004@gmail.com>
2020-07-17 14:53:41 -0700

    Layout: Testing new layout for pagination links

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -282,7 +282,7 @@ export default {
         textDecorationStyle: 'none !important',
       },
       textDecorationLine: 'none !important',
-      padding: '0px 5px 7px 5px',
+      padding: '0px 5px 7px 0px',
     },
     pre: {
       fontFamily: 'monospace',
